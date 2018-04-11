f4e970a6670c5dd38c485123f45a7c6666ebb255
Varun Raval <vraval48@gmail.com>
2018-04-11 15:37:47 -0400

    added pooling layer and perform flattening for fully connected layer if prev layer not flattened

--- layers.py
+++ layers.py
@@ -12,10 +12,24 @@ class Layer(ABC):
 	name: the name of this layer
 	"""
 
-	def __init__(self, nodes, is_input=False, name=""):
-		self.nodes = nodes
+	def __init__(self, shape_nodes, prev_layer=None, is_input=False, name=""):
+		self.shape_nodes = shape_nodes
+		self.nodes = np.prod(shape_nodes)
 		self.is_input = is_input
 		self.name = name
+
+		self.has_weights = False
+		self.has_bias = False
+
+		if prev_layer is not None:
+			prev_layer.next_layer = self
+			self.prev_layer = prev_layer
+
+		# initializing next layer now so that we can identify last layer
+		self.next_layer = None
+
+		self.require_loss = False
+
 
 	@abstractmethod
 	def calc_activations(self):
@@ -29,8 +43,8 @@ class Layer(ABC):
 class InputLayer(Layer):
 
 
-	def __init__(self, nodes):
-		Layer.__init__(self, nodes, True)
+	def __init__(self, shape_nodes):
+		Layer.__init__(self, shape_nodes, is_input=True)
 
 		# self.activations = tf.get_variable("input_activations",  
 		# 	shape=[self.batch_size, self.nodes], initializer=tf.zeros_initializer())
@@ -55,7 +69,8 @@ class InputLayer(Layer):
 
 class HiddenLayer(Layer):
 
-	def __init__(self, nodes, shape_weights, prev_layer, activation, grad_activation=None, name=""):
+	def __init__(self, shape_nodes, shape_weights, prev_layer, activation, 
+		grad_activation=None, name=""):
 		"""
 			nodes: number of nodes in this layer
 			prev_layer: the layer before this layer (don't use this constructor for input layer)
@@ -63,15 +78,9 @@ class HiddenLayer(Layer):
 			grad_activation: the function used to perform gradient of the activation function
 		"""
 
-		Layer.__init__(self, nodes, name=name)
-		self.prev_layer = prev_layer
+		Layer.__init__(self, shape_nodes, prev_layer=prev_layer, name=name)
 		self.activation = activation
 		self.grad_activation = grad_activation
-
-		prev_layer.next_layer = self
-
-		# initializing next layer now so that we can identify last layer
-		self.next_layer = None
 
 		self.activations = None
 		self.pre_activations = None
@@ -81,6 +90,8 @@ class HiddenLayer(Layer):
 		self.weights = tf.get_variable("weights", 
 			shape=shape_weights, 
 			initializer=tf.random_normal_initializer())
+
+		self.has_weights = True
 
 
 
@@ -114,57 +125,6 @@ class HiddenLayer(Layer):
 			return self.grad_activation(self.get_pre_activations())
 
 
-	def get_weights(self):
-		return self.weights
-
-
-class FullyConnectedLayer(HiddenLayer):
-
-	def __init__(self, nodes, prev_layer, activation, grad_activation=None, name=""):
-		"""
-			nodes: number of nodes in this layer
-			prev_layer: the layer before this layer (don't use this constructor for input layer)
-			activation: the function used to perform activation of the neurons
-			grad_activation: the function used to perform gradient of the activation function
-		"""
-
-		shape_weights = [nodes, prev_layer.nodes]
-
-		HiddenLayer.__init__(self, nodes, shape_weights, prev_layer, activation, grad_activation, name=name)
-
-		self.biases = tf.get_variable("biases",
-			shape=(nodes, 1), 
-			initializer=tf.random_normal_initializer())
-
-
-	def calc_pre_activations(self):
-
-		self.pre_activations = tf.matmul(self.prev_layer.calc_activations(), 
-			self.weights, transpose_b=True) + tf.transpose(self.biases)
-
-		return self.pre_activations
-
-
-	def calc_grad_cost_pre_activation_prev_layer(self):
-		"""
-		Returns: tensor of shape batch_size x previous layer activations
-		"""
-		if not self.prev_layer.is_input:
-			# if self.next_layer is None:
-			# 	cur_layer_grad_cost_pre_activation_fn = self.calc_grad_cost_pre_activation
-			# else:
-			# 	cur_layer_grad_cost_pre_activation_fn = self.next_layer.calc_grad_cost_pre_activation_prev_layer
-
-
-			cur_layer_grad_cost_pre_activation_fn = self.next_layer.calc_grad_cost_pre_activation_prev_layer
-
-			self.prev_layer.grad_cost_pre_activation = \
-			tf.matmul(cur_layer_grad_cost_pre_activation_fn(), self.weights) * \
-			self.prev_layer.calc_grad_activation_pre_activation()
-
-			return self.prev_layer.grad_cost_pre_activation
-
-
 	# have to do for multiple datapoints and multiple weights
 	# for a datapoint, for all the output layer activations, it is same
 
@@ -182,6 +142,95 @@ class FullyConnectedLayer(HiddenLayer):
 		return self.grad_cost_pre_activation
 
 	def get_grad_activation_weight(self):
+		return self.grad_activation_weight
+
+
+	def get_weights(self):
+		return self.weights
+
+
+def flatten_except_first(inp):
+	temp_shape = tf.shape(inp)
+	return tf.reshape(inp, [temp_shape[0], tf.cumprod(temp_shape[1:])[-1]])
+
+
+class FullyConnectedLayer(HiddenLayer):
+
+	def __init__(self, shape_nodes, prev_layer, activation, grad_activation=None, name=""):
+		"""
+			nodes: number of nodes in this layer
+			prev_layer: the layer before this layer (don't use this constructor for input layer)
+			activation: the function used to perform activation of the neurons
+			grad_activation: the function used to perform gradient of the activation function
+		"""
+
+		shape_weights = [shape_nodes[0], prev_layer.nodes]
+
+		HiddenLayer.__init__(self, shape_nodes, shape_weights, prev_layer, activation, 
+			grad_activation, name=name)
+
+		self.biases = tf.get_variable("biases",
+			shape=(shape_nodes[0], 1), 
+			initializer=tf.random_normal_initializer())
+
+		self.has_bias = True
+
+
+	def calc_pre_activations(self):
+
+		prev_layer_activations = self.prev_layer.calc_activations()
+
+		# flatten here and repack during grad cost pre activation
+		# also flatten during grad_cost_pre_activation (grad_activation_pre_activation)
+		# and during grad_activation_weight
+
+		# retain 1st dimension and flatten rest
+		prev_layer_activations = tf.cond(tf.not_equal(tf.size(self.prev_layer.shape_nodes), 1), 
+			lambda: flatten_except_first(prev_layer_activations), lambda: prev_layer_activations)
+
+		self.pre_activations = tf.add(tf.matmul(prev_layer_activations, 
+					self.weights, transpose_b=True), tf.transpose(self.biases))
+
+		return self.pre_activations
+
+
+	def calc_grad_cost_pre_activation_prev_layer(self):
+		"""
+		Returns: tensor of shape batch_size x previous layer activations
+		"""
+		if not self.prev_layer.is_input:
+			cur_layer_grad_cost_pre_activation_fn = \
+			self.next_layer.calc_grad_cost_pre_activation_prev_layer
+
+			prev_layer_grad_act_pre_act = self.prev_layer.calc_grad_activation_pre_activation()
+			prev_layer_grad_act_pre_act = \
+			tf.cond(tf.not_equal(tf.size(self.prev_layer.shape_nodes), 1), 
+				lambda: flatten_except_first(prev_layer_grad_act_pre_act), 
+				lambda: prev_layer_grad_act_pre_act)
+
+			prev_layer_grad_cost_pre_activation = \
+			tf.multiply(tf.matmul(cur_layer_grad_cost_pre_activation_fn(), self.weights),
+				prev_layer_grad_act_pre_act)
+
+			self.prev_layer.grad_cost_pre_activation = \
+			tf.cond(tf.not_equal(tf.size(self.prev_layer.shape_nodes), 1), 
+				lambda: tf.reshape(prev_layer_grad_cost_pre_activation, 
+					tf.shape(self.prev_layer.get_pre_activations())), 
+				lambda: prev_layer_grad_cost_pre_activation)
+
+			return self.prev_layer.grad_cost_pre_activation
+
+	def calc_grad_activation_weight(self):
+		"""
+		Returns: tensor with shape (batch_size) x (previous layer units)
+		"""
+		# return tf.assign(self.grad_activation_weight, self.prev_layer.get_activations())
+
+		grad_activation_weight = self.prev_layer.get_activations()
+
+		self.grad_activation_weight = tf.cond(tf.not_equal(tf.size(self.prev_layer.shape_nodes), 1), 
+			lambda: flatten_except_first(grad_activation_weight), lambda: grad_activation_weight)
+
 		return self.grad_activation_weight
 
 
@@ -213,28 +262,36 @@ class FullyConnectedLayer(HiddenLayer):
 		"""
 		Returns: tensor with shape same as bias of this layer
 		"""
-		return tf.transpose(tf.reduce_sum(self.get_grad_cost_pre_activation(), axis=0, keepdims=True))
+		return tf.transpose(tf.reduce_sum(self.get_grad_cost_pre_activation(), axis=0, 
+			keepdims=True))
 
 
 class ConvolutionLayer(HiddenLayer):
 
-	def __init__(self, shape_input, shape_filter, prev_layer, activation, grad_activation=None, name=""):
+	def __init__(self, shape_filter, strides, prev_layer, activation, grad_activation=None, 
+		name=""):
 		"""
-			feature_maps: number of nodes in this layer
+			shape_filter: [1, height, width, 1]
 			prev_layer: the layer before this layer (don't use this constructor for input layer)
 			activation: the function used to perform activation of the neurons
 			grad_activation: the function used to perform gradient of the activation function
 		"""
 
-		out_height = shape_input[1] - shape_filter[0] + 1
-		out_width = shape_input[2] - shape_filter[0] + 1
+		# TODO check
+		out_height = ((prev_layer.shape_nodes[0] - shape_filter[0]) / strides[1]) + 1
+		out_width = ((prev_layer.shape_nodes[1] - shape_filter[1]) / strides[2]) + 1
+		out_channels = shape_filter[-1]
 
-		nodes = out_height * out_width * shape_filter[-1]
+		shape_nodes = [out_height, out_width, out_channels]
 
-		HiddenLayer.__init__(self, nodes, shape_weights, prev_layer, activation, grad_activation, name=name)
+		shape_weights = shape_filter
 
-		self.shape_input = shape_input
+		HiddenLayer.__init__(self, shape_nodes, shape_weights, prev_layer, activation, 
+			grad_activation, name=name)
+
 		self.shape_filter = shape_filter
+
+		# shape_bias = [shape_filter[0], shape_filter[1], ]
 
 		# self.biases = tf.get_variable("biases",
 		# 	shape=(nodes, 1), 
@@ -243,9 +300,10 @@ class ConvolutionLayer(HiddenLayer):
 
 	def calc_pre_activations(self):
 
-		self.pre_activations = tf.nn.conv2d(input=self.prev_layer.features, filter=self.weights,
-											strides=[1, 1, 1, 1], padding='VALID')
+		self.pre_activations = tf.nn.conv2d(input=self.prev_layer.calc_activations(), 
+			filter=self.weights, strides=[1, 1, 1, 1], padding='VALID')
 
+		self.shape_input = tf.shape(prev_layer.get_activations())
 		return self.pre_activations
 
 
@@ -254,32 +312,66 @@ class ConvolutionLayer(HiddenLayer):
 		Returns: tensor of shape batch_size x previous layer activations
 		"""
 		if not self.prev_layer.is_input:
-			cur_layer_grad_cost_pre_activation_fn = self.next_layer.calc_grad_cost_pre_activation_prev_layer
+			cur_layer_grad_cost_pre_activation_fn = \
+			self.next_layer.calc_grad_cost_pre_activation_prev_layer
 
 			self.prev_layer.grad_cost_pre_activation = \
-			tf.nn.conv2d_backprop_input(input_sizes=self.shape_input, filter=self.weights, 
-				out_backprop=cur_layer_grad_cost_pre_activation_fn(), strides=[1, 1, 1, 1], 
-				padding='VALID') * \
-			self.prev_layer.calc_grad_activation_pre_activation()
+			tf.multiply(tf.nn.conv2d_backprop_input(input_sizes=self.shape_input, 
+				filter=self.weights, out_backprop=cur_layer_grad_cost_pre_activation_fn(), 
+				strides=[1, 1, 1, 1], padding='VALID'),
+			self.prev_layer.calc_grad_activation_pre_activation())
 
 			return self.prev_layer.grad_cost_pre_activation
 
 
-	# def calc_grad_activation_weight(self):
-	# 	"""
-	# 	Returns: tensor with shape (batch_size) x (previous layer units)
-
-	# 	"""
-	# 	# return tf.assign(self.grad_activation_weight, self.prev_layer.get_activations())
-
-	# 	self.grad_activation_weight = self.prev_layer.get_activations()
-
-	# 	return self.grad_activation_weight
+	def calc_grad_cost_weight(self):
+		"""
+		Returns: tensor with shape same as weights of this layer
+		"""
+		return tf.nn.conv2d_backprop_filter(input=self.prev_layer.get_activations(), 
+			filter_sizes=self.shape_filter, out_backprop=self.get_grad_cost_pre_activation(),
+			strides=[1, 1, 1, 1], padding='VALID')
 
 
-	def get_grad_cost_pre_activation(self):
-		return self.grad_cost_pre_activation
+class PoolingLayer(Layer):
+
+	def __init__(self, prev_layer, k_size, strides):
+		"""
+		prev_layer: previous layer
+		k_size: kernel size with which to perform max pooling
+		"""
+
+		out_height = ((prev_layer.shape_nodes[0] - k_size[1]) / strides[1]) + 1
+		out_width = ((prev_layer.shape_nodes[1] - k_size[2]) / strides[2]) + 1
+		out_channels = prev_layer.out_channels
+		shape_nodes = [out_width, out_height, out_channels]
+
+		Layer.__init__(self, shape_nodes, prev_layer=prev_layer)
+
+		self.k_size = k_size
+		self.strides = self.strides
+
+		self.require_loss = True
+		layer.loss = None
 
 
-	def get_grad_activation_weight(self):
-		return self.grad_activation_weight
+	def calc_pre_activations():
+		self.activations = tf.nn.max_pool(value=self.prev_layer.calc_activations(), 
+			ksize=self.k_size, strides=self.strides, padding=self.padding)
+
+		self.shape_input = tf.shape(self.prev_layer.get_activations())
+		return self.activations
+
+	def get_activations():
+		return self.activations
+
+
+	def calc_grad_cost_pre_activation_prev_layer(self):
+		"""
+		Returns: tensor of shape batch_size x previous layer activations
+		"""
+		self.next_layer.calc_grad_cost_pre_activation_prev_layer()
+		self.prev_layer.grad_cost_pre_activation = \
+		tf.gradients(self.loss, self.prev_layer.get_pre_activations())
+
+		return self.prev_layer.calc_grad_cost_pre_activation_prev_layer
